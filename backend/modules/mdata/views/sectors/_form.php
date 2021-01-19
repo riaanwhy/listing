bcaf925c68de1436a5605bee97b635ee3f1a9907
HubGoni <76200894+HubGoni@users.noreply.github.com>
2021-01-19 15:36:50 +0700

    crud sectors
    update mdata

---
+++ backend/modules/mdata/views/sectors/_form.php
@@ -0,0 +1,25 @@
+<?php
+
+use yii\helpers\Html;
+use yii\widgets\ActiveForm;
+
+/* @var $this yii\web\View */
+/* @var $model backend\modules\mdata\models\Sectors */
+/* @var $form yii\widgets\ActiveForm */
+?>
+
+<div class="sectors-form">
+
+    <?php $form = ActiveForm::begin(); ?>
+
+    <?= $form->field($model, 'parent_id')->textInput() ?>
+
+    <?= $form->field($model, 'name')->textInput(['maxlength' => true]) ?>
+
+    <div class="form-group">
+        <?= Html::submitButton('Save', ['class' => 'btn btn-success']) ?>
+    </div>
+
+    <?php ActiveForm::end(); ?>
+
+</div>
---
+++ backend/modules/mdata/views/sectors/update.php
@@ -0,0 +1,21 @@
+<?php
+
+use yii\helpers\Html;
+
+/* @var $this yii\web\View */
+/* @var $model backend\modules\mdata\models\Sectors */
+
+$this->title = 'Update Sectors: ' . $model->name;
+$this->params['breadcrumbs'][] = ['label' => 'Sectors', 'url' => ['index']];
+$this->params['breadcrumbs'][] = ['label' => $model->name, 'url' => ['view', 'id' => $model->id]];
+$this->params['breadcrumbs'][] = 'Update';
+?>
+<div class="sectors-update">
+
+    <h1><?= Html::encode($this->title) ?></h1>
+
+    <?= $this->render('_form', [
+        'model' => $model,
+    ]) ?>
+
+</div>
