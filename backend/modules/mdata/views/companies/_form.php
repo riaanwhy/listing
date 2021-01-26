6a6a56a1123024135c291a83d4dabec63d043e5a
riaanwhy <riaanwhy@gmail.com>
2021-01-26 14:21:32 +0700

    select di form
    udate select di form

--- backend/modules/mdata/views/companies/_form.php
+++ backend/modules/mdata/views/companies/_form.php
@@ -3,6 +3,10 @@
 use yii\helpers\Html;
 use yii\widgets\ActiveForm;
 
+
+use yii\helpers\ArrayHelper;
+use kartik\select2\Select2;
+use backend\modules\mdata\models\Countries;
 /* @var $this yii\web\View */
 /* @var $model backend\modules\mdata\models\Companies */
 /* @var $form yii\widgets\ActiveForm */
@@ -20,21 +24,22 @@ use yii\widgets\ActiveForm;
 
     <?= $form->field($model, 'sub_sector')->textInput() ?>
 
-    <?= $form->field($model, 'country')->textInput() ?>
+    <?php //echo $form->field($model, 'country')->textInput() ?>
+
+    <?php echo $form->field($model, 'country')->label(false)->widget(Select2::classname(), ['data' => ArrayHelper::map(Countries::find()->asArray()->all(), 'id', 'name'),
+           'options' => ['placeholder' => 'Project'],
+            'pluginOptions' => [
+                'width'=>'200px',
+                'allowClear' => true,                               
+                                ],
+            ])->label("Contry");?>
+            
 
     <?= $form->field($model, 'exchange')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'website')->textInput(['maxlength' => true]) ?>
 
     <?= $form->field($model, 'profile')->textarea(['rows' => 6]) ?>
-
-    <?= $form->field($model, 'created_at')->textInput() ?>
-
-    <?= $form->field($model, 'updated_at')->textInput() ?>
-
-    <?= $form->field($model, 'created_by')->textInput() ?>
-
-    <?= $form->field($model, 'updated_by')->textInput() ?>
 
     <div class="form-group">
         <?= Html::submitButton(Yii::t('app', 'Save'), ['class' => 'btn btn-success']) ?>
